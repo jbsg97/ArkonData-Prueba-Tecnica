b72e425295f92eac4c6f4a2f6ad0ee7bdd817e96
Bryan <jbsg97@gmail.com>
2021-04-14 18:28:29 -0600

    Se agrega requirements y conector a base de datos

--- src/server.py
+++ src/server.py
@@ -1,7 +1,10 @@
 from flask import Flask
-
+from src.config.db import db
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
 
 @app.route("/")
 def index():
     return "<h1>Bienvenidos a la api de ArkonData</h1>"
+
+db.init_app(app)
